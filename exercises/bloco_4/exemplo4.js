64cdf87541874ead48dab6be717969bea1c06ed8
jhenriquemargrafm <jh.margraf@gmail.com>
2021-03-25 16:13:33 -0300

    parte 4

---
+++ exercises/bloco_4/exemplo4.js
@@ -0,0 +1,14 @@
+let estado = "Outro";
+switch (estado) {
+    case ("Aprovada"):
+        console.log("Aprovado");
+        break;
+    case ("Lista"):
+        console.log("Em recuperação");
+        break;
+    case ("Reprovada"):
+        console.log("reprovado");
+        break;
+    default:
+        console.log("Não se aplica")
+}
