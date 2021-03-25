01954d529fec8293ddb23d3edbfd6f3f1c237bd3
jhenriquemargrafm <jh.margraf@gmail.com>
2021-03-25 15:31:51 -0300

    exemplo parte2

---
+++ exercises/bloco_4/exemplo1.js
@@ -0,0 +1,7 @@
+const name = "José Henrique Margraf Melo";
+const birthCity = "São Paulo";
+const birthYear = 1990;
+console.log(name);
+// erro let birthCity = "Rio" - não pode atribuir outro valor a uma constante
+console.log(birthCity);
+console.log(birthYear);
