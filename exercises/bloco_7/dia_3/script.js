484c7169c60192950ab39835419995ff07bc230b
jhenriquemargrafm <jh.margraf@gmail.com>
2021-05-08 16:35:42 -0300

    revisao

--- exercises/bloco_7/dia_3/script.js
+++ exercises/bloco_7/dia_3/script.js
@@ -1,17 +1,18 @@
-// const assert = require('assert');
+const assert = require('assert');
 
-//function sum(a, b) {
-//  if (typeof a !== 'number' || typeof b !== 'number') {
-//    throw Error('parameters must be numbers');
-//  }
-//  return a + b;
-//}
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw Error('parameters must be numbers');
+  }
+  return a + b;
+}
 
-//assert.strictEqual(typeof sum, 'function');
+assert.strictEqual(typeof sum, 'function');
 
-//assert.strictEqual(sum(4, 5), 9);
-//assert.strictEqual(sum(0, 0), 0);
-//assert.throws(() => {
+assert.strictEqual(sum(4, 5), 9);
+assert.strictEqual(sum(0, 0), 0);
+
+// assert.throws(() => {
 //  sum(4, '5');
 //});
 //assert.throws(() => {
@@ -43,22 +44,21 @@
 
 //const assert = require('assert');
 
-//function myRemoveWithoutCopy(arr, item) {
-//  for (let index = 0, len = arr.length; index < len; index += 1) {
-//    if (arr[index] === item) {
-//      arr.splice(index, 1);
-//      index -= 1;
-//      len -= 1;
-//    }
-//  }
+function myRemoveWithoutCopy(arr, item) {
+  for (let index = 0, len = arr.length; index < len; index += 1) {
+    if (arr[index] === item) {
+      arr.splice(index, 1);
+      index -= 1;
+      len -= 1;
+    }
+  }
+  return arr;
+}
 
-//  return arr;
-//}
+const myList = [1, 2, 3, 4];
 
-//const myList = [1, 2, 3, 4];
-
-//assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4]);
-//assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4]);
+assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4]);
 
 //myRemoveWithoutCopy(myList, 1);
 //assert.strictEqual(myList.length, 3);
