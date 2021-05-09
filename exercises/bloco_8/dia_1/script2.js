30ab659b701502a306e12b5b40db16be266adce4
jhenriquemargrafm <jh.margraf@gmail.com>
2021-05-08 23:42:59 -0300

    bloco8dia2

---
+++ exercises/bloco_8/dia_1/script2.js
@@ -0,0 +1,39 @@
+const mage = {
+  healthPoints: 130,
+  intelligence: 45,
+  mana: 125,
+  damage: undefined,
+};
+
+const warrior = {
+  healthPoints: 200,
+  strength: 30,
+  weaponDmg: 2,
+  damage: undefined,
+};
+
+const dragon = {
+  healthPoints: 350,
+  strength: 50,
+  damage: undefined,
+};
+
+const battleMembers = { mage, warrior, dragon };
+
+//Dano causado pelo dragão
+const dragonAttack = (dragon) => {
+  const minDmg = 15;
+  const { strength } = dragon;
+  const dragonDmg = Math.floor((Math.random() * (strength - minDmg) + minDmg));
+  return dragonDmg;
+};
+console.log(dragonAttack(dragon));
+
+// Dano causado pelo Warrior
+const warriorAttack = (warrior) => {
+  const { weaponDmg } = warrior;
+  const minDmg = warrior.strength;
+  const maxDmg = minDmg * weaponDmg;
+  const warriorDamage = Math.floor((Math.random() * (maxDmg - minDmg)) + minDmg);
+  return warriorDamage;
+};
